b0dae3c71e6ed9295b4c5d9ea16c28c5cca8abfc
lethanhtam190198 <tamle011998@gmail.com>
2022-08-13 09:03:11 +0700

    a

--- ss2/bai_tap/fibonacci/src/fibonacci.ts
+++ ss2/bai_tap/fibonacci/src/fibonacci.ts
@@ -7,6 +7,7 @@ for (let i = 0; i < 10; i++) {
 console.log("Total:" + total)
 
 function fibonacci(n: number): number {
-    if (n == 0 || n == 1) return 1;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    if (n == 0) return 0;
+    if (n <= 2) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2)
 }
