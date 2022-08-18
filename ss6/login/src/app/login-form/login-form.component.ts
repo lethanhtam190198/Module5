3cc728acd7c36d02fd31f0ded1fba989f75caf4d
lethanhtam190198 <tamle011998@gmail.com>
2022-08-18 15:20:50 +0700

    a

---
+++ ss6/login/src/app/login-form/login-form.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    country: new FormControl('', [Validators.required]),
+    age: new FormControl('', [Validators.required, Validators.min(18)]),
+    gender: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required, Validators.pattern('^\\+84\\d{9,10}$')])
+  }, this.checkConfirm);
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+
+  }
+
+  checkConfirm(item: AbstractControl) {
+    const pass = item.value.password;
+    const confirm = item.value.confirmPassword;
+    if (pass !== confirm) {
+      return {confirmpass: true};
+    }
+    return null;
+  }
+
+}
---
+++ ss6/loginform/src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+  }
+}
